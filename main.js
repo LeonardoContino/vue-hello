6d0ce18233afb53632b830412372bee40e5e48ff
LeonardoContino <leoconti2000@hotmail.it>
2023-01-09 15:09:40 +0100

    scritto messaggio in js con data

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
-console.log("js ok");
+// console.log("js ok");
 /*
 Descrizione:
 Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
 Bonus:
 Aggiungere alla pagina un’immagine, presa anch’essa da un data.
 */
+
+const app = Vue.createApp({
+  data() {
+    return {
+      message: "ciao Mondo",
+    };
+  },
+});
